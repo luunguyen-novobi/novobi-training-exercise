704bb755fdb646213d0b1f010599c432454906cd
Luu Nguyen <luu.nguyen@novobi.com>
2022-07-20 14:19:30 +0700

    [IMP] Archive/Unarchive a record logic

--- models/purchase.py
+++ models/purchase.py
@@ -1,9 +1,22 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     active = fields.Boolean(default=True)
 
-    def button_archive(self):
-        pass
+    def action_archive_purchase_orders(self):
+        for record in self:
+            if record.state == 'done' or record.state == 'cancel':
+                record.active = False
+            else:
+                raise UserError('Only allow archive the locked or canceled purchase orders')
+    
+    def action_unarchive_purchase_orders(self):
+        for record in self:
+            if record.state == 'done' or record.state == 'cancel':
+                record.active = True
+            else:
+                raise UserError('Only allow unarchive the locked or canceled purchase orders')
+                
