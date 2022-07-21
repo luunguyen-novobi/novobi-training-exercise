9d07d1c05038f9277335eca08edc313e258b63fa
Luu Nguyen <luu.nguyen@novobi.com>
2022-07-21 14:22:23 +0700

    [IMP] Add report pivot view

--- report/purchase_report.py
+++ report/purchase_report.py
@@ -4,3 +4,9 @@ class PurchaseOrderReport(models.Model):
     _inherit = 'purchase.report'
 
     payment_term_id = fields.Many2one('account.payment.term', 'Payment Terms', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+
+    def _select(self):
+        return super(PurchaseOrderReport, self)._select() + ", po.payment_term_id as payment_term_id"
+
+    def _group_by(self):
+        return super(PurchaseOrderReport, self)._group_by() + ", payment_term_id"
