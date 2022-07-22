c8472da2e4201fb9ad26124a1e219b791eb704bb
Luu Nguyen <luu.nguyen@novobi.com>
2022-07-22 13:48:25 +0700

    [FIX] Change lifespan settings to the shorter way

--- models/purchase_settings.py
+++ models/purchase_settings.py
@@ -3,14 +3,4 @@ from odoo import models, fields
 class PurchaseOrderSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    lifespan = fields.Integer(default=0)
-
-    def set_values(self):
-        super(PurchaseOrderSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('purchase.order.enhancement.settings.lifespan', self.lifespan)
-
-    def get_values(self):
-        res = super(PurchaseOrderSettings, self).get_values()
-        lifespan = self.env['ir.config_parameter'].get_param('purchase.order.enhancement.settings.lifespan')
-        res.update({'lifespan': lifespan})
-        return res
+    lifespan = fields.Integer(default=0, config_parameter='purchase.order.enhancement.settings.lifespan')
